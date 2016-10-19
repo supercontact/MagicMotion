001ed6bd9c880ea3f132cb38c098642f90667415
yali zhu <yali.zhu@shopline.fr>
2016-10-19 16:53:07 +0200

    Add AIs.

---
+++ Assets/Scripts/AI1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI1 : BasicAI {
+	private float lastThinkTime;
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update();
+	}
+
+	public override void PreAttackAction(Unit target) {
+
+	}
+	public override void AttackAction(Unit target) {
+		ReceiveDamage(20, target);
+	}
+
+	public override void DieAction() {
+
+	}
+
+}
