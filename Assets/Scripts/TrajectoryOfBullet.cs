788f1e4f5f03cf1c5e92b22a60d0a1643b79762c
yali zhu <yali.zhu@shopline.fr>
2016-10-20 21:28:52 +0200

    Fix Bullet’s Trajectory

--- Assets/Scripts/TrajectoryOfBullet.cs
+++ Assets/Scripts/TrajectoryOfBullet.cs
@@ -9,43 +9,77 @@ public class TrajectoryOfBullet: MonoBehaviour
 	public int team = 0;
 	public float distanceMax = 10.0f;
 	public float distanceMin = 0.0f;
-	public float speedInit = 5.0f;
+	public float speedInit = 1.0f;
 	public float damage = 10.0f;
+	public float decayTimer = 2.0f;
 
 	protected CharacterController controller;
 
-	private Quaternion targetRotation;
 	private Vector3 targetPosition;
-	private Vector3 attackerPosition;
+	private Vector3 positionInit;
+	private bool isAttacked = false;
+	private float distanceToTarget;
+	private float remainingDistance;
 
 	void Start() {
 		controller = GetComponent<CharacterController>();
 		targetPosition = target.transform.position;
-		attackerPosition = attacker.transform.position;
+		positionInit = attacker.transform.position;
+		distanceToTarget = Vector3.Distance (targetPosition,positionInit);
+		remainingDistance = distanceToTarget < distanceMax ? distanceToTarget : distanceMax;
 	}
 
 	void Update() {
-
-		Launch ();
+		if (team != target.team) {
+			Launch ();
+		}
+		if (isAttacked) {			
+			decayTimer -= Time.deltaTime;
+			if (decayTimer <= 0) {
+				Destroy (gameObject);
+				return;
+			}
+		}
 
 	}
 		
 
 	private void Launch()
 	{
-		float distanceToTarget = Vector3.Distance (targetPosition,transform.position);
+		
 		if (distanceToTarget <= distanceMax) {
 			// Pursuing
 			float moveDistance = speedInit * Time.deltaTime;
-			if (moveDistance > distanceToTarget) {
-				controller.Move (distanceToTarget * (targetPosition - transform.position).normalized);
+			if (remainingDistance <= moveDistance) {
+				controller.Move (remainingDistance * (targetPosition - positionInit).normalized);
+				remainingDistance = 0;
+				transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+				isAttacked = true;
 			} else {
-				controller.Move (moveDistance * (targetPosition - transform.position).normalized);
+				controller.Move (moveDistance * (targetPosition - positionInit).normalized);
+				remainingDistance -= moveDistance;
+			}
+			if (remainingDistance == 0) {
+				controller.Move(5 * Vector3.down * Time.deltaTime * Time.deltaTime);
 			}
 
-			targetRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(targetPosition - transform.position, Vector3.up));
-
+		} else {
+			float moveDistance = speedInit * Time.deltaTime;
+			if (remainingDistance <= moveDistance) {
+				controller.Move (remainingDistance * (targetPosition - positionInit).normalized);
+				remainingDistance = 0;
+				transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+				isAttacked = true;
+			} else {
+				controller.Move (moveDistance * (targetPosition - positionInit).normalized);
+				remainingDistance -= moveDistance;
+			}
+			if (remainingDistance == 0) {
+				controller.Move(5 * Vector3.down * Time.deltaTime * Time.deltaTime);
+			}
 		}
+
+		
 	}
 
 	private void AcquireTargetPosition()
